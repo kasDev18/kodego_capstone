9b649572e9d8406f3402b241317e5d93d716d702
kasDev18 <kascatindoy@yahoo.com>
2022-07-31 00:46:11 +0800

    Added Position content

--- src/component/Landing/Landing.js
+++ src/component/Landing/Landing.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Position from '../Position/Position';
 import styles from './Landing.module.css';
 import LandingHeader from './LandingHeader/LandingHeader';
 
 const Landing = () => (
   <div className={styles.Landing}>
     <LandingHeader />
+    <div className={styles.content + ' d-flex p-3 px-5'}>
+      <div className={styles.Position + ' border rounded col-2 mx-1'}>
+        <Position />
+      </div>
+      <div className={styles.Position + ' border rounded col-8 '}>
+        <Position />
+      </div>
+      <div className={styles.Position + ' border rounded col-2 mx-1'}>
+        <Position />
+      </div>
+    </div>
   </div>
 );
 
---
+++ src/component/Position/Position.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Position.module.css';
+
+class Position extends Component {
+  position = [
+    { position: "Supervisor" },
+    { position: "Assistant Supervisor" },
+    { position: "Calibration Officer" },
+    { position: "QA Analyst" },
+    { position: "Technical Engineer" },
+    { position: "Ink and Chemical Clerk" },
+    { position: "Inspector" }
+  ];
+  position_array = {};
+
+  CreatePositionArray() {
+    this.position_array = this.position.map((element) => (
+      <div className={styles.Position_designation + ' text-dark p-2'}>
+        <h6 className={styles + ' pt-1'}>{element.position}</h6>
+      </div>
+    ))
+  }
+
+  render() {
+    this.CreatePositionArray();
+    return (
+      <div className={styles.Position_content + ' p-3'}>
+        <div className={styles + ' d-flex justify-content-center text-success p-3'}>
+          <h5 className="text-uppercase fw-bolder text-decoration-underline">Position</h5>
+        </div>
+        <div>
+          {this.position_array}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+export default Position;
