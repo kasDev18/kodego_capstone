9de39d473e5793a4a74fafc48df789814fc642b2
kasDev18 <kascatindoy@yahoo.com>
2022-08-04 02:05:05 +0800

    Changing background-color of Forms QMR on click

--- src/component/FormsContent/FormsContent.js
+++ src/component/FormsContent/FormsContent.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './FormsContent.module.css';
 import QMR from './QMR/QMR';
 
-const FormsContent = () => (
-  <div className={styles.FormsContent + '  col-8 container p-2'}>
-    <QMR />
-  </div >
-);
-
+class FormsContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false
+    }
+  }
+  render() {
+    return (
+      <div className={styles.FormsContent + '  col-8 container p-2'}>
+        {this.state.display ? <QMR /> : this.state.display}
+      </div >
+    )
+  }
+}
 
 export default FormsContent;
--- src/component/Landing/Landing.js
+++ src/component/Landing/Landing.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Forms from '../Forms/Forms';
 import FormsContent from '../FormsContent/FormsContent';
 import Position from '../Position/Position';
 import styles from './Landing.module.css';
 import LandingHeader from './LandingHeader/LandingHeader';
 
-const Landing = () => (
-  <div className={styles.Landing}>
-    <LandingHeader />
-    <div className={styles.content + ' d-flex p-3 px-5'}>
-      <Position />
-      <FormsContent />
-      <Forms />
-    </div>
-  </div>
-);
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: true,
+    };
+
+    this.ChangeBackground = this.ChangeBackground.bind(this);
+  }
+
+  ChangeBackground() {
+    this.setState({ hidden: !this.state.hidden });
+  }
+
+  render() {
+    return (
+      <div className={styles.Landing}>
+        <LandingHeader />
+        <div className={styles.content + ' d-flex p-3 px-5'}>
+          <Position />
+          <FormsContent />
+          <div className={styles.Forms_cont + ' border rounded col-2 mx-1'}>
+            <div className={styles.Forms_Records_content + ' p-3'}>
+              <div className={styles + ' d-flex justify-content-center text-success p-3'}>
+                <h5 className="text-uppercase fw-bolder text-decoration-underline">Forms/Records</h5>
+              </div>
+              <div className={styles.Forms_Records + ' text-dark p-2'} style={{
+                backgroundColor: this.state.hidden ? 'transparent' : 'green'
+              }}>
+                < h6 className={styles.random + ' pt-1 d-flex justify-content-end'} onClick={this.ChangeBackground} style={{
+                  color: this.state.hidden ? '#000' : '#fff'
+                }}
+                >QMR(Paper)</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div >
+    )
+  }
+}
 
 export default Landing;
