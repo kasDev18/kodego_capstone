7d12943468c1dc76f1e18a9dd2ea268fd6614e47
kasDev18 <kascatindoy@yahoo.com>
2022-09-02 23:26:04 +0800

    Catching username from database when logged

--- src/component/Landing/LandingHeader/LandingHeader.js
+++ src/component/Landing/LandingHeader/LandingHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './LandingHeader.module.css';
 import logo from './images/1525506679828.jpg';
 import profile from './images/th.png';
@@ -6,41 +6,51 @@ import LanguageSelector from '../../LanguageSelector/LanguageSelector';
 import axios from 'axios';
 import i18next from 'i18next'
 
-const LandingHeader = () => (
-  <div className={styles.LandingHeader + '  d-flex justify-content-center align-items-center'}>
-    <div className={styles.LandingHeader_col_1 + ' col-7 d-flex align-items-center'}>
-      <div className={styles + ' mx-2'}>
-        <img src={logo} className="img-fluid border rounded-circle" alt="..." width={"40px"} height={"40px"} />
-      </div>
-      <div className={styles + ' text-white mx-2 pt-2'}>
-        <h6>{i18next.t('fpc_qa_pms')}</h6>
-      </div>
-    </div>
-    <div className={styles + ' col d-flex justify-content-end align-items-center px-5'}>
-      <LanguageSelector />
-      <div className={styles.LandingHeader_settings + ' dropdown'}>
-        <i className="fa-solid fa-user text-light"></i>
-        <a className={styles.LandingHeader_settings_dropdown + ' btn btn-transparent border border-0 dropdown-toggle'} href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-          {i18next.t('Username')}
-        </a>
+const LandingHeader = () => {
+  const [username, setUsername] = useState(localStorage.getItem('user-logged'));
 
-        <ul className="dropdown-menu shadow" aria-labelledby="dropdownMenuLink">
-          <div className={styles.LandingHeader_profile + ' d-flex justify-content-center p-2'}>
-            <img src={profile} className="img-fluid m-3 border rounded-circle" alt="..." width={"70px"} height={"70px"} />
-          </div>
-          <div className={styles.LandingHeader_prof_details + ' text-light'}>
-            <div className={styles + ' d-flex justify-content-center'}>
-              <h6>{i18next.t('ID: #123456')}</h6>
-            </div>
-            <div className={styles + ' d-flex justify-content-center'}>
-              <h6>{i18next.t('Username')}</h6>
-            </div>
-          </div>
-          <li className={styles + ' px-5'}><a className="dropdown-item text-warning text-center border border-warning" href="https://fpc-qa-login.netlify.app/">{i18next.t('Logout')}</a></li>
-        </ul>
+  // useEffect(() => {
+  //   setUsername(() => {
+  //     localStorage.getItem('user-logged')
+  //   });
+  // })
+
+  return (
+    < div className={styles.LandingHeader + '  d-flex justify-content-center align-items-center'} >
+      <div className={styles.LandingHeader_col_1 + ' col-7 d-flex align-items-center'}>
+        <div className={styles + ' mx-2'}>
+          <img src={logo} className="img-fluid border rounded-circle" alt="..." width={"40px"} height={"40px"} />
+        </div>
+        <div className={styles + ' text-white mx-2 pt-2'}>
+          <h6>{i18next.t('fpc_qa_pms')}</h6>
+        </div>
       </div>
-    </div>
-  </div>
-);
+      <div className={styles + ' col d-flex justify-content-end align-items-center px-5'}>
+        <LanguageSelector />
+        <div className={styles.LandingHeader_settings + ' dropdown'}>
+          <i className="fa-solid fa-user text-light"></i>
+          <a className={styles.LandingHeader_settings_dropdown + ' btn btn-transparent border border-0 dropdown-toggle'} href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+            {username}
+          </a>
+
+          <ul className="dropdown-menu shadow" aria-labelledby="dropdownMenuLink">
+            <div className={styles.LandingHeader_profile + ' d-flex justify-content-center p-2'}>
+              <img src={profile} className="img-fluid m-3 border rounded-circle" alt="..." width={"70px"} height={"70px"} />
+            </div>
+            <div className={styles.LandingHeader_prof_details + ' text-light'}>
+              <div className={styles + ' d-flex justify-content-center'}>
+                <h6>{i18next.t('ID: #123456')}</h6>
+              </div>
+              <div className={styles + ' d-flex justify-content-center'}>
+                <h6>{username}</h6>
+              </div>
+            </div>
+            <li className={styles + ' px-5'}><a className="dropdown-item text-warning text-center border border-warning" href="https://fpc-qa-login.netlify.app/">{i18next.t('Logout')}</a></li>
+          </ul>
+        </div>
+      </div>
+    </div >
+  )
+};
 
 export default LandingHeader;
